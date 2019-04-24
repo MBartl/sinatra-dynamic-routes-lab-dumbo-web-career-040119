a71be762a6fa2ef171960969da4be83d1e7b3015
Matthew Bartl <mattbartl@gmail.com>
2019-04-24 13:59:41 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -6,5 +6,11 @@ class App < Sinatra::Base
     @name = params[:name]
     @name.reverse
   end
+  
+  get '/square/:number' do
+    @number = params[:number].to_i
+    @output = @number ** 2
+    @output.to_s
+  end
 
 end
